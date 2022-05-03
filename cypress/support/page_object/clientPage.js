78734e3314ca8a05ec4a2a42a5fe58ef9c0df241
Iana Pastukhova <monstervan221188@gmail.com>
2022-05-02 17:53:56 -0700

    add gitignore

--- cypress/support/page_object/clientPage.js
+++ cypress/support/page_object/clientPage.js
@@ -1,12 +1,3 @@
 
-export class ClientPage {
 
-  createClient(){
-  //   cy.get('[href="/client"]')
-  //     .click()
-  //   cy.get('[data-qa="create-client-button"]')
-  //     .click()
-  //   cy.get('[data-qa="page-title"]').should('contain', 'Create New Client')
-  // }
-}
 
